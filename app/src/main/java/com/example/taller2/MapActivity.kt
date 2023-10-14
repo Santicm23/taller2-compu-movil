b376cd089d1c8b11d6f676277614c9963d976a33
Santicm23 <santicastrom@outlook.com>
2023-10-13 21:15:38 -0500

    mapa

--- app/src/main/java/com/example/taller2/MapActivity.kt
+++ app/src/main/java/com/example/taller2/MapActivity.kt
@@ -1,38 +1,53 @@
 package com.example.taller2
 
-import android.content.DialogInterface
+import android.Manifest
+import android.content.pm.PackageManager
 import android.hardware.Sensor
 import android.hardware.SensorEvent
 import android.hardware.SensorEventListener
 import android.hardware.SensorManager
 import android.location.Geocoder
-import androidx.appcompat.app.AppCompatActivity
+import android.location.Location
 import android.os.Bundle
-import android.view.View
-
+import android.util.Log
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import com.example.taller2.databinding.ActivityMapBinding
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationCallback
+import com.google.android.gms.location.LocationResult
+import com.google.android.gms.location.LocationServices
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.GoogleMap.OnMyLocationButtonClickListener
+import com.google.android.gms.maps.LocationSource.OnLocationChangedListener
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.LatLng
-import com.google.android.gms.maps.model.MarkerOptions
-import com.example.taller2.databinding.ActivityMapBinding
 import com.google.android.gms.maps.model.MapStyleOptions
+import com.google.android.gms.maps.model.MarkerOptions
 import com.google.android.gms.maps.model.PolylineOptions
 import java.io.IOException
 
-class MapActivity : AppCompatActivity(), OnMapReadyCallback {
+class MapActivity : AppCompatActivity(), OnMapReadyCallback, OnMyLocationButtonClickListener, OnLocationChangedListener {
 
     private lateinit var mMap: GoogleMap
     private lateinit var binding: ActivityMapBinding
     private lateinit var sensorManager: SensorManager
     private lateinit var lightSensor: Sensor
-    private var lightSensorListener: SensorEventListener? = null
+    private lateinit var lightSensorListener: SensorEventListener
     // Initialize the geocoder
     private lateinit var mGeocoder: Geocoder
-    private var polyline: PolylineOptions? = null
+    private lateinit var polyline: PolylineOptions
+    private var mFusedLocationClient: FusedLocationProviderClient? = null
 
     private var light: Boolean = true
+
+    companion object {
+        private const val LOCATION_PERMISSION_REQUEST_CODE = 0
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -126,7 +141,7 @@ class MapActivity : AppCompatActivity(), OnMapReadyCallback {
                 if (addressString.isNotEmpty()) {
                     try {
                         val addresses = mGeocoder.getFromLocationName(addressString, 2)
-                        if (addresses != null && !addresses.isEmpty()) {
+                        if (!addresses.isNullOrEmpty()) {
                             val addressResult = addresses[0]
                             val position = LatLng(addressResult.latitude, addressResult.longitude)
                             mMap.addMarker(
@@ -143,54 +158,126 @@ class MapActivity : AppCompatActivity(), OnMapReadyCallback {
             }
             false
         }
+
+        binding.switch1.setOnCheckedChangeListener { buttonView, isChecked ->
+            if (isChecked) {
+                enableLocation()
+            } else {
+                if (ContextCompat.checkSelfPermission(
+                        this,
+                        Manifest.permission.ACCESS_FINE_LOCATION
+                    ) == PackageManager.PERMISSION_GRANTED) {
+                    mMap.isMyLocationEnabled = false
+                }
+            }
+        }
+        mFusedLocationClient = LocationServices.getFusedLocationProviderClient(this)
     }
 
     override fun onResume() {
         super.onResume()
 
-        if (lightSensorListener != null) {
-            sensorManager.registerListener(
-                lightSensorListener,
-                lightSensor,
-                SensorManager.SENSOR_DELAY_NORMAL
-            )
-        }
+        if (!::lightSensorListener.isInitialized) return
+        sensorManager.registerListener(
+            lightSensorListener,
+            lightSensor,
+            SensorManager.SENSOR_DELAY_NORMAL
+        )
     }
 
     override fun onPause() {
         super.onPause()
+        if (!::lightSensorListener.isInitialized) return
 
-        if (lightSensorListener != null) {
-            sensorManager.unregisterListener(lightSensorListener)
+        sensorManager.unregisterListener(lightSensorListener)
+    }
+
+    private fun enableLocation() {
+        if (!::mMap.isInitialized) return
+        if (ContextCompat.checkSelfPermission(
+                this,
+                Manifest.permission.ACCESS_FINE_LOCATION
+            ) == PackageManager.PERMISSION_GRANTED) {
+            mMap.isMyLocationEnabled = true
+            polyline = PolylineOptions()
+            mMap.addPolyline(polyline)
+        } else {
+            requestLocationPermission()
         }
     }
 
-    /*private fun findAddress() {
-        val addressString = mAddress.text.toString()
-        if (addressString.isNotEmpty()) {
-            try {
-                val addresses = mGeocoder.getFromLocationName(addressString, 2,
-                    lowerLeftLatitude, lowerLeftLongitude,
-                    upperRightLatitude, upperRightLongitude
-                )
-                if (addresses != null && !addresses.isEmpty()) {
-                    val addressResult = addresses[0]
-                    val position = LatLng(addressResult.latitude, addressResult.longitude)
-                    mMap.addMarker(
-                        MarkerOptions().position(position)
-                            .title(addressResult.featureName)
-                            .snippet(addressResult.getAddressLine(0))
-                            .icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_AZURE))
-                    )
-                    mMap.moveCamera(CameraUpdateFactory.newLatLng(position))
-                } else {
-                    Toast.makeText(this, "Dirección no encontrada", Toast.LENGTH_SHORT).show()
-                }
-            } catch (e: IOException) {
-                e.printStackTrace()
-            }
+    private fun requestLocationPermission() {
+        if (ActivityCompat.shouldShowRequestPermissionRationale(
+                this,
+                Manifest.permission.ACCESS_FINE_LOCATION
+            )
+        ) {
+            Toast.makeText(
+                this,
+                "Se necesita el permiso para mostrar la ubicación actual",
+                Toast.LENGTH_SHORT
+            ).show()
         } else {
-            Toast.makeText(this, "La dirección está vacía", Toast.LENGTH_SHORT).show()
+            ActivityCompat.requestPermissions(
+                this,
+                arrayOf(Manifest.permission.ACCESS_FINE_LOCATION),
+                1
+            )
         }
-    }*/
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        when(requestCode) {
+            LOCATION_PERMISSION_REQUEST_CODE -> {
+                if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    if (ContextCompat.checkSelfPermission(
+                            this,
+                            Manifest.permission.ACCESS_FINE_LOCATION
+                        ) == PackageManager.PERMISSION_GRANTED) {
+                        mMap.isMyLocationEnabled = true
+                        polyline = PolylineOptions()
+                        mMap.addPolyline(polyline)
+                    }
+                } else {
+                    Toast.makeText(
+                        this,
+                        "No se concedió el permiso para mostrar la ubicación actual",
+                        Toast.LENGTH_SHORT
+                    ).show()
+                }
+                return
+            }
+            else -> {}
+        }
+    }
+
+    override fun onResumeFragments() {
+        super.onResumeFragments()
+        if (!::mMap.isInitialized) return
+        if (ContextCompat.checkSelfPermission(
+                this,
+                Manifest.permission.ACCESS_FINE_LOCATION
+            ) == PackageManager.PERMISSION_GRANTED) {
+            mMap.isMyLocationEnabled = true
+            polyline = PolylineOptions()
+            mMap.addPolyline(polyline)
+        } else {
+            binding.switch1.isChecked = false
+        }
+    }
+
+    override fun onMyLocationButtonClick(): Boolean {
+        Toast.makeText(this, "Ubicación actual", Toast.LENGTH_SHORT).show()
+        return false
+    }
+    override fun onLocationChanged(p0: Location) {
+        polyline.add(LatLng(p0.latitude, p0.longitude))
+        mMap.addPolyline(polyline)
+        Log.i("Location", "Location changed")
+    }
 }
